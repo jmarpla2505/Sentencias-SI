6bd2569d959067c8ff336bfb96ce9e7668bd8e99
José Ángel <al_jmartosa@iessierradelasvillas.org>
2022-10-13 09:51:23 +0200

    Terminada actividad 3

---
+++ Ejercicio3.py
@@ -0,0 +1,7 @@
+#Escribe un programa que lea un número e indique si es par o impar.
+numero = 0
+numero = int(input("Dime un número:"))
+if (numero /2 and numero%2==0):
+    print ("El número es par")
+else:
+    print("El número es impar")
