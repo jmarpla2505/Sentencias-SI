0d40af9cd5a70757597053ca595d2fbc44fd8831
José Ángel <al_jmartosa@iessierradelasvillas.org>
2022-10-13 09:51:05 +0200

    Terminada actividad 1

---
+++ Ejercicio1.py
@@ -0,0 +1,10 @@
+#Algoritmo que pida dos números e indique si el primero es mayor que el segundo o no.
+
+numero1 = 0
+numero2 = 0
+numero1 = int(input("Dime un número:"))
+numero2 = int(input("Dime otro número:"))
+if (numero1>numero2):
+    print("El número 1 es mayor que el 2")
+else:
+    print("El número 2 es mayor que el 1")
