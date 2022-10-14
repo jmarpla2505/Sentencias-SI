f9d9bb322a11f20770d3aa5968f338de5e1dc64e
José Ángel <al_jmartosa@iessierradelasvillas.org>
2022-10-14 16:31:05 +0200

    Ej 9 definitivo

--- Ejercicio9.py
+++ Ejercicio9.py
@@ -3,19 +3,14 @@
 numero1 = 0
 numero2 = 0
 numero3 = 0
-numero1 = int(input("Dime un número:"))
-numero2 = int(input("Dime otro número:"))
-numero3 = int(input("Dime un tercer número:"))
+ListaNumeros = []   
+numero1 = (int)(input("Dime un número:\n"))
+numero2 = (int)(input("Dime otro número:\n"))
+numero3 = (int)(input("Dime otro número:\n"))
 
-if (numero1>numero2>numero3):
-    print(numero1,numero2,numero3)
-elif (numero2>numero1>numero3):
-    print(numero2,numero1,numero3)
-elif (numero3>numero1>numero2):
-    print(numero3,numero1,numero3)
-elif (numero1>numero3>numero2):
-    print(numero1,numero3,numero2)
-elif (numero2>numero3>numero1):
-    print(numero2,numero3,numero1)
-elif (numero3>numero2>numero1):
-    print(numero3,numero2,numero1)
+ListaNumeros.insert(0, numero1)
+ListaNumeros.insert(1, numero2)
+ListaNumeros.insert(2, numero3)
+ordenados = sorted(ListaNumeros, reverse=True)
+
+print("El orden sería", ordenados)
